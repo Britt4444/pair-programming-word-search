540e503063601211914ab159e6c576873c348bd4
Brittany Cowper <brittferren@gmail.com>
2023-03-24 13:35:04 -0500

    Successfully modified wordSearch function to find word vertically, horizontally, and in reverse; created corresponding tests that passed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programming Anthony Chambers and Britt Cowper
 
-module.exports = wordSearch
+const wordSearch = function(
+  letters,
+  word,
+  vertChoice = false,
+  reverseChoice = false
+) {
+  let matrix = letters;
+  let wordChoice = word;
+  //pass array through transpose function if vertChoice is truthy
+  if (vertChoice) {
+    matrix = transpose(letters);
+  }
+  //turn word into reverse array if reverseChoice is truthy
+  if (reverseChoice) {
+    wordChoice = word.split("").reverse().join("");
+  }
+  const horizontalJoin = matrix.map((ls) => ls.join(""));
+  for (const l of horizontalJoin) {
+    if (l.includes(wordChoice)) return true;
+  }
+  return false;
+};
+
+const transpose = function(matrix) {
+  const newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      newMatrix[i].push(matrix[j][i]);
+    }
+  }
+  return newMatrix;
+};
+
+module.exports = { wordSearch };
